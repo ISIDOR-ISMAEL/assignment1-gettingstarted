6a7e0f576099684c162021f10100b73da04956ac
ISIDORISMAEL <it2284@nyu.edu>
2025-02-05 20:16:51 -0500

    Commit without the secret phrase

--- gettingStarted.py
+++ gettingStarted.py
@@ -1,35 +1,43 @@
-### welcome_assignment_answers
-### Input - All nine questions given in the assignment.
-### Output - The right answer for the specific question.
+import hashlib
 
 def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
-    if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
-        ### you should understand why this else case should be included
-        ### what happens if there is a typo in one of the questions?
-        ### maybe put something here to flag an issue and catch errors
-        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
-    return(answer)
+    match question.lower():  # Convert input to lowercase for case-insensitive matching
+        case "in slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a ta?":
+            answer = "XXXXXXXXXXXXXXXX"
+        case "are encoding and encryption the same? - yes/no":
+            answer = "No"
+        case "is it possible to decrypt a message without a key? - yes/no":
+            answer = "No"
+        case "is it possible to decode a message without a key? - yes/no":
+            answer = "Yes"
+        case "is a hashed message supposed to be un-hashed? - yes/no":
+            answer = "No"
+        case "what is the sha256 hashing value of your nyu email and use the answer in your code - ":
+            nyu_email = input("Enter your NYU email: ")
+            answer = hashlib.sha256(nyu_email.encode()).hexdigest()
+        case "is md5 a secured hashing algorithm? - yes/no":
+            answer = "No"
+        case "what layer of the tcp/ip model does the protocol dns belong to? - the answer should be an integer number":
+            answer = 5
+        case "what layer of the tcp/ip model does the protocol icmp belong to? - the answer should be an integer number":
+            answer = 3
+        case _:  # Default case
+            answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+    return answer
 # Complete all the questions.
 
 
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
-    debug_question = "Are encoding and encryption the same? - Yes/No"
+    debug_question = "What is the SHA256 hashing value of your NYU email and use the answer in your code - "
     print(welcome_assignment_answers(debug_question))
 
 #Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+#1"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+#2"Are encoding and encryption the same? - Yes/No":
+#3"Is it possible to decrypt a message without a key? - Yes/No":
+#4"Is it possible to decode a message without a key? - Yes/No":
+#5"Is a hashed message supposed to be un-hashed? - Yes/No":
+#6"What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+#7"Is MD5 a secured hashing algorithm? - Yes/No":
+#8"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+#9"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
